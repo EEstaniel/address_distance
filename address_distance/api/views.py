5cbc7fab02cfa91bfed33c02186a6c2910012383
EEstaniel <edwineestaniel93@gmail.com>
2023-02-12 21:37:52 -0800

    added comments/docstring

--- address_distance/api/views.py
+++ address_distance/api/views.py
@@ -6,18 +6,31 @@ from .config import key
 
 @api_view(['POST'])
 def distance(request):
-    # Google API Key for Distance Matrix API
+    """
+    POST request that takes in a request with JSON data: origin and destination.
+    This then converts the addresses into a geocode in order to retrieve the place_id to use for the url request.
+    This then returns data regarding the addresses and includes the distance between them.
 
+    :param request: {origin: [address1], destination: [address2]}
+    :return: Json data
+    """
+
+    # initialize googlemaps api key
     gmaps = googlemaps.Client(key=key)
 
+    # convert origin and destination to geocodes to retrieve the location's place_id.
     geocode_1 = gmaps.geocode(request.data['origin'])
     geocode_2 = gmaps.geocode(request.data['destination'])
 
+    # initialize url to use Google's Distance Matrix API
     url = f"https://maps.googleapis.com/maps/api/distancematrix/json?origins=place_id:{geocode_1[0]['place_id']}&destinations=place_id:{geocode_2[0]['place_id']}&key={key}"
+
+    # initialize payload, headers, and response.
     payload = {}
     headers = {}
-
     response = requests.request("GET", url, headers=headers, data=payload)
 
+    # return JSON data
+    # distance = response.text["rows"][0]['elements'][0]['distance']['text']
     return Response(response.text)
 
